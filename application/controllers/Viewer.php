9e0ba140905295410d43540a5a07e81d3eb4db83
Master Yoda <jim_parry@bcit.ca>
2016-02-15 11:05:51 -0800

    Update for winter 2016 Signed-off-by:Master Yoda <jim_parry@bcit.ca>

--- application/controllers/Viewer.php
+++ application/controllers/Viewer.php
@@ -9,29 +9,29 @@
  */
 class Viewer extends Application {
 
-    function __construct()
-    {
-	parent::__construct();
-    }
+	function __construct()
+	{
+		parent::__construct();
+	}
 
-    //-------------------------------------------------------------
-    //  The normal pages
-    //-------------------------------------------------------------
+	//-------------------------------------------------------------
+	//  The normal pages
+	//-------------------------------------------------------------
 
-    function index()
-    {
-	$this->data['pagebody'] = 'homepage';    // this is the view we want shown
-	$this->data['authors'] = $this->quotes->all();
-	$this->render();
-    }
+	function index()
+	{
+		$this->data['pagebody'] = 'homepage';	// this is the view we want shown
+		$this->data['authors'] = $this->quotes->all();
+		$this->render();
+	}
 
-    // method to display just a single quote
-    function quote($id)
-    {
-	$this->data['pagebody'] = 'justone';    // this is the view we want shown
-	$this->data = array_merge($this->data, (array) $this->quotes->get($id));
-	$this->render();
-    }
+	// method to display just a single quote
+	function quote($id)
+	{
+		$this->data['pagebody'] = 'justone';	// this is the view we want shown
+		$this->data = array_merge($this->data, (array) $this->quotes->get($id));
+		$this->render();
+	}
 
 }
 
--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -9,21 +9,21 @@
  */
 class Welcome extends Application {
 
-    function __construct()
-    {
-	parent::__construct();
-    }
+	function __construct()
+	{
+		parent::__construct();
+	}
 
-    //-------------------------------------------------------------
-    //  The normal pages
-    //-------------------------------------------------------------
+	//-------------------------------------------------------------
+	//  The normal pages
+	//-------------------------------------------------------------
 
-    function index()
-    {
-	$this->data['pagebody'] = 'justone';    // this is the view we want shown
-	$this->data = array_merge($this->data, (array) $this->quotes->last());
-	$this->render();
-    }
+	function index()
+	{
+		$this->data['pagebody'] = 'justone';	// this is the view we want shown
+		$this->data = array_merge($this->data, (array) $this->quotes->last());
+		$this->render();
+	}
 
 }
 
